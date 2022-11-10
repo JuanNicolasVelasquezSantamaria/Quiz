6c736388f44dfdb657a973c297e38a7dae812845
JuanNicolasVelasquezSantamaria <115585089+JuanNicolasVelasquezSantamaria@users.noreply.github.com>
2022-11-10 18:00:39 -0500

    Create quiz.py
    quiz

---
+++ quiz.py
@@ -0,0 +1,36 @@
+# Quiz
+Quiz10/11/22
+
+
+#punto 1
+r = ['Nilo', "Magdalena", "Danubio", "Amazonas", "Bravo"]
+p = ["Egipto", "Colombia", "Alemania", "Colombia", "Mexico"]
+for i in range(r._len_()):
+    print(f"El {r[i]} corre atraves de {p[i]}")
+print("##############################################")
+print("Rios: ")
+for i in range(r._len_()):
+    print(f"{r[i]}")
+print("##############################################")
+print("Paises: ")
+for j in range(p._len_()):
+    print(f"{p[j]}")
+    
+    
+#punto 2
+print("\t---------------Bienvenido a mi glosario---------------")
+print("\t      las palabras que a lo largo del semestre son:")
+
+#palabras
+print("\n>Java:\n\tPlataforma informática de lenguaje de programación creada por Sun Microsystems en 1995")
+
+print("\n>Python:\n\tEs un lenguaje de programación orientado a objetos de alto nivel y fácil de interpretar con sintaxis fácil de leer")
+
+print("\n>Eficiencia:\n\tEs la facultad de conseguir un resultado optimizando el uso de los recursos.\n\tTambién puede referirse a la realización de un trabajo en un periodo de tiempo más corto")
+
+print("\n>Eficacia:\n\tEs la realización de las cosas correctamente, con el simple propósito de lograr o alcanzar las metas previstas.")
+
+print("\n>Respeto:\n\tEl respeto es un valor y una cualidad positiva que se refiere a la acción de respetar;\n\tes equivalente a tener veneración, aprecio y reconocimiento por una persona o cosa")
+
+print("\nFin del glosario.")
+    
